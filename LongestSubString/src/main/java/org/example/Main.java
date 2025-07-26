9e135b827eaaed880a85932805eaf5f2a27addfb
Md. Reazul Haque <37473674+reazulhaque20@users.noreply.github.com>
2025-07-27 05:04:08 +0800

    Longest SubString

---
+++ LongestSubString/src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring("bbbb"));
+    }
+}
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        boolean[] flag = new boolean[256];
+
+        int result = 0;
+        int start = 0;
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            char current = arr[i];
+            if (flag[current]) {
+                result = Math.max(result, i - start);
+                for (int k = start; k < i; k++) {
+                    if (arr[k] == current) {
+                        start = k + 1;
+                        break;
+                    }
+                    flag[arr[k]] = false;
+                }
+            } else {
+                flag[current] = true;
+            }
+        }
+        result = Math.max(arr.length - start, result);
+
+        return result;
+    }
+}
